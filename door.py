15e4adf093b0eda9a439639f1fa3ea9f58b3b55a
Herman Schistad <hermansc@samfundet.no>
2014-05-01 15:28:50 +0200

    Make the script use interrupts, instead of sleeping + checking

--- door.py
+++ door.py
@@ -6,32 +6,37 @@ API_HOST = "hackerspace.idi.ntnu.no"
 API_ENDPOINT = "/api/door"
 GPIO_PIN = 7
 
-def check_door(state):
-  # Read state from GPIO.
-  gpio = GPIO.input(GPIO_PIN)
+class Door:
+  # Initialize state to 1 (door closed)
+  state = 1
 
-  if state != gpio:
-    # State has changed, create connection to API.
-    conn = httplib.HTTPConnection(API_HOST)
+  def check():
+    # Read state from GPIO.
+    gpio = GPIO.input(GPIO_PIN)
 
-    if gpio == 1:
-      conn.request('PUT', API_ENDPOINT)
-      print "Door closed"
-    else:
-      conn.request('POST', API_ENDPOINT)
-      print "Door open"
-  return gpio
+    if self.state != gpio:
+      # State has changed, create connection to API.
+      conn = httplib.HTTPConnection(API_HOST)
+
+      if gpio == 1:
+        conn.request('PUT', API_ENDPOINT)
+        print "Door closed"
+      else:
+        conn.request('POST', API_ENDPOINT)
+        print "Door open"
+    self.state = gpio
 
 if __name__ == '__main__':
   # Initialize the GPIO.
   GPIO.setmode(GPIO.BOARD)
   GPIO.setup(GPIO_PIN, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
 
-  # Initialize state to 0.
-  state = 0
+  # Class representing a door, and its state.
+  door = Door()
 
+  # Add interrupt handler for the GPIO.
+  GPIO.add_event_detect(GPIO_PIN, GPIO.RISING, callback=lambda x: d.check(), bouncetime=300)
+
+  # Run forever.
   while True:
-    # Sleep between checks, and run forever.
-    state = check_door(state)
-    time.sleep(TIMEOUT)
-
+    pass
